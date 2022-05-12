0bca7cb5a724a824f1ed13b50c53d1188477c1b6
ZeinabAlbri <105216052+ZeinabAlbri@users.noreply.github.com>
2022-05-12 22:46:16 +0200

    Add files via upload

---
+++ hom1A.py
@@ -0,0 +1,6 @@
+list=['zeinab','abeer','hala','amaar','reem','sameer','hasan']
+name=input('enter the student name to show if he is agraduate or not:')
+if name in list:
+    print ('this user is graduated')
+else:
+    print('this user is not graduated')
